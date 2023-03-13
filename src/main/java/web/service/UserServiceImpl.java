9495addbd8623855222ad980c0b581e52e434857
Andrey Pechenkin <mcreature@inbox.ru>
2023-03-13 21:27:46 +0300

    Реализован interface UserService.

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,4 +1,33 @@
 package web.service;
 
+import web.model.User;
+
+import java.util.List;
+
 public class UserServiceImpl implements UserService{
+
+    @Override
+    public void saveUser(User user) {
+        
+    }
+
+    @Override
+    public void updateUser(User updateUser) {
+
+    }
+
+    @Override
+    public void removeUserById(int id) {
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return null;
+    }
 }
