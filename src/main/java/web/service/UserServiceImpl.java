bb9f31e7b0c57f06f6d9ae167eca0bd06ba11527
Andrey Pechenkin <mcreature@inbox.ru>
2023-03-19 11:11:28 +0300

    Аннотация @Transactional перенесена в сервис.

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.model.User;
 
@@ -14,16 +15,19 @@ public class UserServiceImpl implements UserService{
     private UserDao userDao;
 
     @Override
+    @Transactional
     public void saveUser(User user) {
         userDao.saveUser(user);
     }
 
     @Override
+    @Transactional
     public void updateUser(User updateUser) {
         userDao.updateUser(updateUser);
     }
 
     @Override
+    @Transactional
     public void removeUserById(Long id) {
         userDao.removeUserById(id);
     }
